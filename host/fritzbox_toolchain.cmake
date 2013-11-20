a46be07da10a41375b4ea84318f54344b82173e6
Jonas Wielicki <j.wielicki@sotecware.net>
2013-11-20 13:18:28 +0100

    Bootstrap host code

---
+++ host/fritzbox_toolchain.cmake
@@ -0,0 +1,4 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+include(CMakeForceCompiler)
+
+CMAKE_FORCE_C_COMPILER(mips-linux-uclibc-gcc GNU)
---
+++ host/hintd.c
@@ -0,0 +1,8 @@
+#include <pthread.h>
+
+#include "common/comm.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
