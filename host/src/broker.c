20f4459f2ca30714903e0939ee0197fa4faa9785
Jonas Wielicki <j.wielicki@sotecware.net>
2013-12-01 20:57:52 +0100

    Extend broker to create screens

--- host/src/broker.c
+++ host/src/broker.c
@@ -8,10 +8,154 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+
+#include "lpcdisplay.h"
+#include "timestamp.h"
+
+#define TABBAR_LEFT ((LCD_WIDTH-1)-SCREEN_MARGIN_RIGHT)
+#define TABBAR_TOP (SCREEN_CLIENT_AREA_TOP+4)
+
+void broker_enqueue_new_task_at(
+    struct broker_t *broker,
+    task_func_t func,
+    const struct timespec *run_at,
+    void *userdata);
+void broker_enqueue_new_task_in(
+    struct broker_t *broker,
+    task_func_t func,
+    uint32_t in_msec,
+    void *userdata);
+void broker_enqueue_task(struct broker_t *broker, struct task_t *task);
+struct task_t *broker_get_next_task(struct broker_t *broker);
+void broker_handle_touch_down(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y);
+void broker_handle_touch_move(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y);
+void broker_handle_touch_up(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y);
+void broker_init(struct broker_t *broker, struct comm_t *comm);
+void broker_process_lpc_message(
+    struct broker_t *broker, struct lpc_msg_t *msg);
+void broker_process_message(struct broker_t *broker, void *item);
+void broker_repaint_screen(
+    struct broker_t *broker);
+void broker_repaint_tabbar(
+    struct broker_t *broker);
+void broker_repaint_time(
+    struct broker_t *broker);
+void broker_run_next_task(struct broker_t *broker);
+void broker_switch_screen(
+    struct broker_t *broker,
+    int new_screen);
+int broker_tab_hit_test(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y);
+void *broker_thread(struct broker_t *state);
+bool broker_update_time(
+    struct broker_t *broker,
+    struct timespec *next_run,
+    void *userdata);
+
+
+void broker_enqueue_new_task_at(
+    struct broker_t *broker,
+    task_func_t func,
+    const struct timespec *run_at,
+    void *userdata)
+{
+    struct task_t *new_task = malloc(sizeof(struct task_t));
+    new_task->func = func;
+    memcpy(&new_task->run_at, run_at, sizeof(struct timespec));
+    new_task->userdata = userdata;
+    broker_enqueue_task(broker, new_task);
+}
+
+void broker_enqueue_new_task_in(
+    struct broker_t *broker,
+    task_func_t func,
+    uint32_t in_msec,
+    void *userdata)
+{
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+    timestamp_add_msec(&now, in_msec);
+    broker_enqueue_new_task_at(broker, func, &now, userdata);
+}
+
+void broker_enqueue_task(struct broker_t *broker, struct task_t *task)
+{
+    // FIXME: implement a binary search here
+    for (intptr_t i = 0; i < array_length(&broker->tasks); i++) {
+        const struct task_t *other_task = array_get(&broker->tasks, i);
+        if (timestamp_less(
+            &other_task->run_at,
+            &task->run_at))
+        {
+            array_push(&broker->tasks, i, task);
+            return;
+        }
+    }
+    array_push(&broker->tasks, INTPTR_MAX, task);
+}
+
+struct task_t *broker_get_next_task(struct broker_t *broker)
+{
+    if (array_length(&broker->tasks) == 0) {
+        return NULL;
+    }
+
+    return array_get(&broker->tasks, -1);
+}
+
+void broker_handle_touch_down(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y)
+{
+    broker->touch_is_up = false;
+    int new_screen = broker_tab_hit_test(broker, x, y);
+    if ((new_screen != -1) && (new_screen != broker->active_screen)) {
+        broker_switch_screen(broker, new_screen);
+    }
+}
+
+void broker_handle_touch_move(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y)
+{
+
+}
+
+void broker_handle_touch_up(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y)
+{
+    broker->touch_is_up = true;
+}
 
 void broker_init(struct broker_t *broker, struct comm_t *comm)
 {
     broker->comm = comm;
+    broker->touch_is_up = true;
+    broker->active_screen = 0;
+    array_init(&broker->tasks, 32);
+
+    screen_create(
+        &broker->screens[SCREEN_BUS_MONITOR],
+        comm,
+        "DVB Abfahrtsmonitor",
+        "DVB");
+    screen_dept_init(&broker->screens[SCREEN_BUS_MONITOR]);
+
+    screen_create(
+        &broker->screens[SCREEN_WEATHER_INFO],
+        comm,
+        "Wetterdaten",
+        "Enviro");
+    screen_weather_init(&broker->screens[SCREEN_WEATHER_INFO]);
 
     pthread_create(
         &broker->thread, NULL,
@@ -19,15 +163,22 @@ void broker_init(struct broker_t *broker, struct comm_t *comm)
         broker);
 }
 
-void broker_process_lpc_message(struct lpc_msg_t *msg)
+void broker_process_lpc_message(
+    struct broker_t *broker, struct lpc_msg_t *msg)
 {
     switch (msg->subject) {
     case LPC_SUBJECT_TOUCH_EVENT:
     {
-        fprintf(stderr, "broker: touch event: x=%d; y=%d; z=%d\n",
-                msg->payload.touch_ev.x,
-                msg->payload.touch_ev.y,
-                msg->payload.touch_ev.z);
+        const coord_int_t x = msg->payload.touch_ev.x;
+        const coord_int_t y = msg->payload.touch_ev.y;
+        const coord_int_t z = msg->payload.touch_ev.z;
+        if (broker->touch_is_up && (z > 0)) {
+            broker_handle_touch_down(broker, x, y);
+        } else if ((!broker->touch_is_up) && (z > 0)) {
+            broker_handle_touch_move(broker, x, y);
+        } else if ((!broker->touch_is_up) && (z == 0)) {
+            broker_handle_touch_up(broker, x, y);
+        }
         break;
     }
     default:
@@ -39,7 +190,7 @@ void broker_process_lpc_message(struct lpc_msg_t *msg)
     }
 }
 
-void broker_process_message(void *item)
+void broker_process_message(struct broker_t *broker, void *item)
 {
     struct msg_header_t *hdr = (struct msg_header_t *)item;
     switch (hdr->sender) {
@@ -54,7 +205,7 @@ void broker_process_message(void *item)
         memcpy(&msg, &((uint8_t*)item)[sizeof(struct msg_header_t)],
                hdr->payload_length);
         free(item);
-        broker_process_lpc_message(&msg);
+        broker_process_lpc_message(broker, &msg);
         return;
     }
     case MSG_ADDRESS_ARDUINO:
@@ -73,6 +224,108 @@ void broker_process_message(void *item)
     free(item);
 }
 
+void broker_repaint_screen(
+    struct broker_t *broker)
+{
+    if (broker->active_screen >= 0) {
+        struct screen_t *screen = &broker->screens[broker->active_screen];
+        screen_draw_header(screen);
+        screen_draw_background(screen);
+        screen_repaint(screen);
+    }
+}
+
+void broker_repaint_tabbar(
+    struct broker_t *broker)
+{
+    const coord_int_t tab_x0 = TABBAR_LEFT;
+    coord_int_t tab_y0 = TABBAR_TOP;
+
+    for (int i = 0; i < SCREEN_COUNT; i++) {
+        screen_draw_tab(
+            broker->comm,
+            broker->screens[i].tab_caption,
+            tab_x0,
+            tab_y0,
+            (i == broker->active_screen));
+
+        tab_y0 += TAB_HEIGHT + TAB_PADDING;
+    }
+}
+
+void broker_repaint_time(
+    struct broker_t *broker)
+{
+    time_t timep = time(NULL);
+    struct tm *time = localtime(&timep);
+    static char buffer[6];
+    strftime(buffer, 6, "%H:%M", time);
+    if (time->tm_sec % 2 == 0) {
+        buffer[2] = ' ';
+    }
+    lpcd_fill_rectangle(
+        broker->comm,
+        CLOCK_POSITION_X, 0,
+        LCD_WIDTH-1, CLOCK_POSITION_Y+2,
+        0x0000);
+    lpcd_draw_text(
+        broker->comm,
+        CLOCK_POSITION_X, CLOCK_POSITION_Y,
+        LPC_FONT_DEJAVU_SANS_20PX_BF, 0xffff,
+        buffer);
+}
+
+void broker_run_next_task(struct broker_t *broker)
+{
+    struct task_t *task = array_pop(&broker->tasks, -1);
+    bool run_again = task->func(broker, &task->run_at, task->userdata);
+    if (!run_again) {
+        free(task);
+        return;
+    }
+    broker_enqueue_task(broker, task);
+}
+
+void broker_switch_screen(
+    struct broker_t *broker,
+    int new_screen)
+{
+    if (broker->active_screen != -1) {
+        struct screen_t *screen = &broker->screens[broker->active_screen];
+        screen_hide(screen);
+    }
+    broker->active_screen = new_screen;
+    if (broker->active_screen != -1) {
+        broker_repaint_screen(broker);
+    }
+    broker_repaint_tabbar(broker);
+}
+
+int broker_tab_hit_test(
+    struct broker_t *broker,
+    coord_int_t x, coord_int_t y)
+{
+    y -= TABBAR_TOP;
+    x -= TABBAR_LEFT;
+    if ((x < 0) || (x >= TAB_WIDTH)) {
+        return -1;
+    }
+    if (y < 0) {
+        return -1;
+    }
+
+    int index = y / (TAB_HEIGHT+TAB_PADDING);
+    if (y - index*(TAB_HEIGHT+TAB_PADDING) > TAB_HEIGHT) {
+        return -1;
+    }
+
+    if (index >= SCREEN_COUNT) {
+        return -1;
+    }
+
+    return index;
+}
+
 void *broker_thread(struct broker_t *state)
 {
 #define FD_COUNT 1
@@ -82,9 +335,29 @@ void *broker_thread(struct broker_t *state)
     pollfds[0].events = POLLIN;
     pollfds[0].revents = 0;
 
+    broker_repaint_screen(state);
+    broker_repaint_tabbar(state);
+    broker_enqueue_new_task_in(
+        state, &broker_update_time, 0, NULL);
+
     while (true)
     {
-        poll(&pollfds[0], FD_COUNT, -1);
+        int32_t timeout = -1;
+        struct task_t *task = broker_get_next_task(state);
+        while (task) {
+            struct timespec curr_time;
+            clock_gettime(CLOCK_MONOTONIC_RAW, &curr_time);
+            timeout = timedelta_in_msec(&task->run_at, &curr_time);
+            if (timeout > 0) {
+                break;
+            } else {
+                broker_run_next_task(state);
+            }
+            task = broker_get_next_task(state);
+        }
+
+        fprintf(stderr, "polling (timeout=%d)\n", timeout);
+        poll(&pollfds[0], FD_COUNT, timeout);
 
         if (pollfds[FD_RECV].revents & POLLIN) {
             char act;
@@ -96,9 +369,20 @@ void *broker_thread(struct broker_t *state)
             }
             void *item = queue_pop(&state->comm->recv_queue);
             assert(item != NULL);
-            broker_process_message(item);
+            broker_process_message(state, item);
         }
     }
 
     return NULL;
+}
+
+bool broker_update_time(
+    struct broker_t *broker,
+    struct timespec *next_run,
+    void *userdata)
+{
+    broker_repaint_time(broker);
+    clock_gettime(CLOCK_MONOTONIC_RAW, next_run);
+    timestamp_add_msec(next_run, 1000);
+    return true;
 }
--- host/src/broker.h
+++ host/src/broker.h
@@ -4,16 +4,54 @@
 #include <pthread.h>
 
 #include "comm.h"
+#include "screen.h"
+
+#define SCREEN_COUNT                    (2)
+#define SCREEN_BUS_MONITOR              (0)
+#define SCREEN_WEATHER_INFO             (1)
+#define SCREEN_MISC                     (2)
+
+#define CLOCK_UPDATE_INTERVAL           (1000)
+
+struct broker_t;
+
+typedef bool(*task_func_t)(
+    struct broker_t *broker,
+    struct timespec *next_run,
+    void *userdata);
+
+struct task_t {
+    task_func_t func;
+    struct timespec run_at;
+    void *userdata;
+};
 
 struct broker_t {
     pthread_t thread;
 
     struct comm_t *comm;
+
+    bool touch_is_up;
+    struct screen_t screens[SCREEN_COUNT];
+    int active_screen;
+
+    struct array_t tasks;
 };
 
-void broker_init(struct broker_t *broker, struct comm_t *comm);
+void broker_enqueue_new_task_at(
+    struct broker_t *broker,
+    task_func_t func,
+    const struct timespec *run_at,
+    void *userdata);
+void broker_enqueue_new_task_in(
+    struct broker_t *broker,
+    task_func_t func,
+    uint32_t in_msec,
+    void *userdata);
+void broker_enqueue_task(struct broker_t *broker, struct task_t *task);
 
-void broker_process_message(void *item);
+
+void broker_init(struct broker_t *broker, struct comm_t *comm);
 
 void *broker_thread(struct broker_t *state);
 
