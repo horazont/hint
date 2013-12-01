04785a43044a09d3301f1afae57e4d6884c993ee
Jonas Wielicki <j.wielicki@sotecware.net>
2013-12-01 20:57:05 +0100

    Update and extend timestamp library

--- host/src/timestamp.c
+++ host/src/timestamp.c
@@ -1,8 +1,22 @@
 #include "timestamp.h"
 
+void timestamp_add_msec(
+    struct timespec *to, uint32_t msec)
+{
+    to->tv_sec += msec/1000;
+    to->tv_nsec += msec%1000;
+    if (to->tv_nsec > 1000000000) {
+        to->tv_sec += 1;
+        to->tv_nsec -= 1000000000;
+    }
+}
+
 uint32_t timedelta_in_msec(
     const struct timespec *a, const struct timespec *b)
 {
+    if (timestamp_less(a, b)) {
+        return 0;
+    }
     if ((a->tv_sec - b->tv_sec) >= (UINT32_MAX/1000)) {
         return UINT32_MAX;
     }
--- host/src/timestamp.h
+++ host/src/timestamp.h
@@ -3,6 +3,29 @@
 
 #include <time.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+static inline bool timestamp_less(
+    const struct timespec *a, const struct timespec *b)
+{
+    if (a->tv_sec < b->tv_sec) {
+        return true;
+    } else if (a->tv_sec == b->tv_sec) {
+        return a->tv_nsec < b->tv_nsec;
+    } else {
+        return false;
+    }
+}
+
+static inline void timestamp_print(
+    const struct timespec *a)
+{
+    fprintf(stderr, "tv_sec=%ld; tv_nsec=%ld;\n", a->tv_sec, a->tv_nsec);
+}
+
+void timestamp_add_msec(
+    struct timespec *to, uint32_t msec);
 
 uint32_t timedelta_in_msec(
     const struct timespec *a, const struct timespec *b);
