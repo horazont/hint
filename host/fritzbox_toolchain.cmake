9da88eadb7f3f926efdbc29306c6749552c30848
Jonas Wielicki <j.wielicki@sotecware.net>
2013-12-05 17:39:09 +0100

    Update CMake to be able to build for the fritzbox

--- host/fritzbox_toolchain.cmake
+++ host/fritzbox_toolchain.cmake
@@ -2,3 +2,14 @@ SET(CMAKE_SYSTEM_NAME Linux)
 include(CMakeForceCompiler)
 
 CMAKE_FORCE_C_COMPILER(mips-linux-uclibc-gcc GNU)
+
+SET(CMAKE_FIND_ROOT_PATH  /home/horazont/Builds/freetz-trunk/toolchain/build/mips_gcc-4.6.3_uClibc-0.9.32.1/mips-linux-uclibc/include)
+#~ include_directories("")
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(PLATFORM "mips-linux-uclibc")
